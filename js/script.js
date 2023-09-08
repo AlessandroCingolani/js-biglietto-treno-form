6f56cdca8bc7ea53a18e2a02ba97b3496946c871
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-08 14:33:20 +0200

    [add]:genbtn eventList

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 /*
-1. Aggiungo variabili per il form
- 
+1. Aggiungo variabili prezzo e sconti
+2. prendo element id name 
  */
+const priceForKm = 0.21;
+const underDiscount = 20;
+const overDiscount = 40;
+const name = document.getElementById('name');
+const btnGener = document.getElementById('btn-gener');
+
+btnGener.addEventListener('click',function(){
+  console.log('adsasd');
+});
+
+
+console.log(name)
+console.log(btnGener)
